446d02d21f4d96f602708250d2207845643c06e4
Begüm Yaren ÖZTÜRK <103455094+yaren0600@users.noreply.github.com>
2025-11-02 23:07:31 +0300

    Add logic to track cells guarded by guards
    Implement guard visibility logic to track guarded cells.

--- 2257.py
+++ 2257.py
@@ -6,3 +6,19 @@ class Solution(object):
         
         #  Görülen hücreleri tutacak set
         guarded = set()
+        
+        #  Her guard için 4 yönü kontrol et
+        for r, c in guards:
+            # Yukarı, aşağı, sol, sağ yönler
+            for dr, dc in [(-1,0), (1,0), (0,-1), (0,1)]:
+                nr, nc = r + dr, c + dc
+                # Yön boyunca ilerle
+                while 0 <= nr < m and 0 <= nc < n and (nr, nc) not in walls_set and (nr, nc) not in guards_set:
+                    guarded.add((nr, nc))
+                    nr += dr
+                    nc += dc
+        
+        #  Sonuç = Toplam boş hücre - görülen hücreler
+        total_cells = m * n
+        occupied = len(guards) + len(walls)
+        return total_cells - occupied - len(guarded)
